9bcfa1c0f64beafec86d78377de1d51f23dbeb12
Abbey <tumbzzc@qq.com>
2018-11-20 16:32:06 +0800

    Update auto_tracker.sh

--- auto_tracker.sh
+++ auto_tracker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_http.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /data/aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /data/aria2/aria2.conf
     echo add......
